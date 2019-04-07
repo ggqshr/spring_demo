2719f8ab362ddb95cafd7cbffa8641ed6bc9356e
ggq <ggq18663278150@sian.com>
2019-04-07 09:09:25 +0800

    天界了注释

--- src/main/java/com/spring_boot/demo/config/SecurityConfig.java
+++ src/main/java/com/spring_boot/demo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.spring_boot.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 在这里配置对http请求的拦截，已经各种权限信息
+     *
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
@@ -25,6 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll();
     }
 
+    /**
+     * 在这里进行授权验证
+     *
+     * @param auth
+     * @throws Exception
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).
@@ -32,5 +45,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("admin")
                 .and()
                 .withUser("swy").password(new BCryptPasswordEncoder().encode("swy")).authorities("user");
+    }
+
+    /**
+     * 在这里配置对一些静态资源的忽略
+     *
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/");
     }
 }
